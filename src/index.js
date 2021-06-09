2a64a91fd6e5f34357ab3fef5aaa2919738e98e4
Marta <marta.villalobos@hackney.gov.uk>
2021-06-09 11:52:02 +0100

    create map function and change basemap

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+//load leaflet components, styles and icons
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+//This fix the default icon issue
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 //load the dotenv config in order to use the process.env variables
 //const regeneratorRuntime = require("regenerator-runtime");
@@ -7,15 +19,23 @@ const axios = require('axios').default;
 //Create the uprn from the url parameter
 let uprn = new URL(location.href).searchParams.get("uprn");
 console.log(uprn);
-
-let map = document.getElementById("map");
+let map = document.getElementById('map');
 
 //Function that create the map
-createMap = () => {
-    console.log("Creating map...");
+function createMapLocation(latitude,longitude){
+    map = L.map('mapDiv').setView([latitude, longitude], 13);
+    L.tileLayer(`https://api.os.uk/maps/raster/v1/zxy/Outdoor_3857/{z}/{x}/{y}.png?key=${process.env.OS_RASTER_API_KEY}`,{
+    fadeAnimation: false,
+    opacity: 1,
+    attribution:
+    'Map data &copy; Crown copyright and database rights 2021 <a href="https://www.ordnancesurvey.co.uk/">Ordnance Survey</a> 100019635.' ,
+    maxZoom: 19,
+    }).addTo(map);
+    let marker = L.marker([latitude, longitude]).addTo(map);
 };
 
-getCoordinates = (uprn) => {
+//Function that get the coordinates from the uprn
+function getCoordinates(uprn) {
     fetch(`${process.env.ADDRESSES_API_PROXY_PROD}?format=detailed&uprn=${uprn}`, {
       method: "get"
     })
@@ -27,14 +47,11 @@ getCoordinates = (uprn) => {
         let longitude=results[0].longitude;
         console.log(latitude);
         console.log(longitude);
-        createMap();
+        createMapLocation(latitude,longitude);
     })
 };
 
-
+//If there is a uprn, we get the coordinates and create a map
 if (!uprn == ''){
     getCoordinates(uprn);
-} else {
-    console.log("UPRN is missing");
 }
-
